37a838e190943feff46a2375ccf107aa548eabcb
Mauro Lucero <mauroaglucero@gmail.com>
2024-05-08 00:37:50 -0300

    4 finalizado

--- lib/funciones.php
+++ lib/funciones.php
@@ -6,4 +6,13 @@ function letra_random() {
     return chr(rand($min, $max));
 }
 
-
+function estaEnArreglo(array $arregloLetras, string $unaLetra) {
+    $letraEnArreglo = false;
+    foreach ($arregloLetras as $value) {
+        if ($value == strtolower($unaLetra)) {
+            $letraEnArreglo = true;
+            break;
+        }
+    }
+    return $letraEnArreglo;
+}
